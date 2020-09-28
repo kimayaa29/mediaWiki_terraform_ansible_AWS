615046b7be5d8ea63d66eaf14430750c792ed274
kimayaa29 <magic.29@rediffmail.com>
2020-09-28 15:48:46 +0530

    networking changes added

--- ec2_provision_mediawiki.tf
+++ ec2_provision_mediawiki.tf
@@ -1,15 +1,15 @@
 resource "aws_instance" "mediawiki" {
     ami = var.ami
     instance_type = var.instance-type
-    user_data = "${data.template_file.user_data.rendered}"
+    subnet_id = aws_subnet.mw_subnet.id
+    vpc_security_group_ids = [aws_security_group.mw_sg.id]
+    user_data = data.template_file.user_data.rendered
     key_name = var.key_name
     tags = {
-        Name = "mediawiki"
+        "Name" = "mediawiki"
     }
 }
 
-
-
 data "template_file" "user_data" {
-    template = "${file("prereq.tpl")}"
+    template = file("prereq.tpl")
 }
