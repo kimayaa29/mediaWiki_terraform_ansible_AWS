15b7252e02e7910dd9768bee70493305e39b4360
kimayaa29 <magic.29@rediffmail.com>
2020-10-01 11:26:30 +0530

    subnet variables added

--- variable.tf
+++ variable.tf
@@ -7,17 +7,25 @@ variable "instance-type"{
 }
 
 variable "vpc-cidr" {
-    default = "10.1.0.0/16"
+    default = "10.0.0.0/16"
 }
 
-variable "subnet-cidr" {
+variable "subnet-cidr-public-1" {
     default = "10.0.0.0/24"
 }
 
-variable "availability_zone" {
+variable "subnet-cidr-public-2" {
+    default = "10.0.1.0/24"
+}
+
+variable "availability_zone_1a" {
     default = "ap-south-1a"
 }
 
+variable "availability_zone_1b" {
+    default = "ap-south-1b"
+
+}
 variable "key_name" {
     default = "AWS_access"
 }
